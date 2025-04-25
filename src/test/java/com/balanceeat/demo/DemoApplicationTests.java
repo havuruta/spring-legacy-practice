f02885c30d7dd2ceaa0c669ad87622a2e53f0f26
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-04-25 11:59:49 +0900

    init

---
+++ src/test/java/com/balanceeat/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balanceeat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
